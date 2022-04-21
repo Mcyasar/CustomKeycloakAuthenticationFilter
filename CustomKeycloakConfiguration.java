a215f84ef9ada6f77c5b1a7a78b6b03592fd3e53
Mcyasar <mcyasar@gmail.com>
2022-04-21 15:38:29 +0300

    Update CustomKeycloakConfiguration.java

--- CustomKeycloakConfiguration.java
+++ CustomKeycloakConfiguration.java
@@ -1,1 +1,22 @@
 
+package org.demo.config;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(value = RUNTIME)
+@Target(value = { TYPE })
+@Configuration
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+@EnableWebSecurity
+public @interface  CustomKeycloakConfiguration {
+    
+}
